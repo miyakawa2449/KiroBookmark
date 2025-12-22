128249596b811b3372f49db8c27735f521c87ec6
Tsuyoshi Miyakawa <t.miyakawa244@gmail.com>
2025-12-22 11:29:04 +0900

    Initial Commit

---
+++ KiroBookmark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KiroBookmark
+//
+//  Created by Tsuyoshi Miyakawa on 2025/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KiroBookmark/KiroBookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiroBookmarkApp.swift
+//  KiroBookmark
+//
+//  Created by Tsuyoshi Miyakawa on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct KiroBookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
